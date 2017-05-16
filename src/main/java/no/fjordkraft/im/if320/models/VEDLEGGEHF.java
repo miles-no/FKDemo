5468a9187622d8be1e8ef53e83ae72b0cd80251f
bhavik shah <bhavik shah>
2017-05-16 10:28:36 +0530

    commented ubl invoice

--- src/main/java/no/fjordkraft/im/if320/models/VEDLEGGEHF.java
+++ src/main/java/no/fjordkraft/im/if320/models/VEDLEGGEHF.java
@@ -34,13 +34,13 @@ import javax.xml.bind.annotation.XmlType;
 * 
 */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "", propOrder = {
+/*@XmlType(name = "", propOrder = {
  "invoice"
-})
+})*/
 @XmlRootElement(name = "VEDLEGG_EHF")
 public class VEDLEGGEHF {
 
- @XmlElement(name = "Invoice", namespace = "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" ,required = true)
+ /*@XmlElement(name = "Invoice", namespace = "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2" ,required = true)
  protected oasis.names.specification.ubl.schema.xsd.invoice_2.Invoice invoice;
 
 public oasis.names.specification.ubl.schema.xsd.invoice_2.Invoice getInvoice() {
@@ -49,7 +49,7 @@ public oasis.names.specification.ubl.schema.xsd.invoice_2.Invoice getInvoice() {
 
 public void setInvoice(oasis.names.specification.ubl.schema.xsd.invoice_2.Invoice invoice) {
 	this.invoice = invoice;
-}
+}*/
 
  
 
