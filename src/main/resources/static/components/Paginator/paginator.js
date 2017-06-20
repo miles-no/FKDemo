e2aecd4b20bb2f7c766b3e6ed8381b2ec53ba35a
kshitijbahul <chunks.3@gmail.com>
2017-06-20 20:49:11 +0530

    WIP : Paginator component Integrated to a page

--- src/main/resources/static/components/Paginator/paginator.js
+++ src/main/resources/static/components/Paginator/paginator.js
@@ -1,22 +1,49 @@
 var app = angular.module('invoiceManagerApp');
-function getReults(){
-
+function getPageData(pageNumber){
+    let queryParams = {
+        page:pageIndex-1,
+        size:this.pageSize
+    }
+    $http.get(this.getUrl,{params}).then(function(result){
+        this.model= result.data[this.key];
+    },function(error){
+        
+    })
 }
-function paginationController($interval,$state){
+function paginationController($http){
     var ctrl = this;
-    ctrl.odometerOptions ={theme: 'plaza', duration: 3000,animation : 'count'};
-        console.log('In the component here ',ctrl,ctrl.theme,ctrl.odometerOptions,ctrl.processingState);
-    ctrl.openDetailedView = function(){
-        console.log('Entered openDetailedView ');
-        $state.go('overview',{"processingState": ctrl.processingState.name});
-    }    
+    ctrl.currentPage=1;
+    //ctrl.totalPages = Math.ceil(ctrl.totalResults/ctrl.pageSize);
+    console.log('In the component here ',ctrl);
+    let getPageData = function(){
+        ctrl.queryParams.page=ctrl.currentPage -1;
+        ctrl.queryParams.size= ctrl.pageSize;
+        
+        $http.get(ctrl.getUrl,{params:ctrl.queryParams}).then(function(result){
+            //ctrl.model= result.data[ctrl.key];
+            ctrl.onPageChange({result: result});
+        },function(error){
+            
+        })
+    }   
+    ctrl.getNextPageData = function(){
+        ctrl.currentPage = ctrl.currentPage + 1 ;
+        getPageData();
+    }
+    ctrl.getPrevPageData = function(){
+        ctrl.currentPage = ctrl.currentPage - 1 ;
+        getPageData();
+    }
 }
 app.component('paginator',{
     templateUrl: 'components/Paginator/paginator.html',
     bindings: {
         pageSize: '<',
-        totalPages :'<',
-        model: '='
+        totalPages :'=',
+        onPageChange: '&',
+        getUrl : '<',
+        key:'<',
+        queryParams :'<'
     },
     controller: paginationController,
 });
