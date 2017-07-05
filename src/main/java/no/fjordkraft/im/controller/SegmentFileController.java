9e97b2139041a5fec4fb37d1f7fbd009215467c9
bhavik shah <bhavik shah>
2017-07-05 11:34:20 +0530

    Add segmentfile details

---
+++ src/main/java/no/fjordkraft/im/controller/SegmentFileController.java
@@ -0,0 +1,37 @@
+package no.fjordkraft.im.controller;
+
+import no.fjordkraft.im.model.SegmentFile;
+import no.fjordkraft.im.services.impl.SegmentFileServiceImpl;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by miles on 6/21/2017.
+ */
+@RestController
+@RequestMapping("/segment/file")
+public class SegmentFileController {
+
+    @Autowired
+    SegmentFileServiceImpl segmentFileService;
+
+    @RequestMapping(value = "",method = RequestMethod.PUT)
+    @ResponseBody
+    public void updateSegmentFile(@RequestParam("id") Long id,
+                                  @RequestPart("fileContent") MultipartFile fileContent) throws IOException {
+
+        byte[] bytes = fileContent.getBytes();
+        String payload = Base64.encodeBase64String(bytes);
+
+        SegmentFile segmentFile = new SegmentFile();
+        segmentFile.setId(id);
+        segmentFile.setFileContent(payload);
+        segmentFileService.updateSegmentFile(segmentFile);
+    }
+}
