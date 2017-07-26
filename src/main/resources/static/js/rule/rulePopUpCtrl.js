d08ea7be73312e763d0fb722ebda22c96cb3b979
Ashu <ashu.gupta@miles.in>
2017-07-26 21:35:52 +0530

    Added rule option

--- src/main/resources/static/js/rule/rulePopUpCtrl.js
+++ src/main/resources/static/js/rule/rulePopUpCtrl.js
@@ -2,7 +2,16 @@ app.controller('ManageRulePopupController',function($scope,options,close){
   $scope.options = options;
   $scope.type = ['STRING','FLOAT','INTEGER']
   $scope.rule = options.body.bodyContent
-  console.log($scope.options)
+  var text = ''
+  if ($scope.rule.options !== null){
+    angular.forEach($scope.rule.options,function(item,index){
+      text = text + item
+      if (index !== ($scope.rule.options.length - 1)){
+        text = text + ', '
+      }
+    })
+    $scope.rule.options = text
+  }
   $scope.dismissModal = function(result){
     close(result,200);
   }
