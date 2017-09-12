2031b843ad3c642c6bf0d51ea6ccd2482d7f8599
bhavik shah <bhavik shah>
2017-09-12 13:20:32 +0530

    enable security

--- InvoiceManagerUI/src/main/java/no/fjordkraft/im/InvoiceManagerUIApplication.java
+++ InvoiceManagerUI/src/main/java/no/fjordkraft/im/InvoiceManagerUIApplication.java
@@ -4,17 +4,21 @@ import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-@SpringBootApplication(exclude = {LiquibaseAutoConfiguration.class,})
+@SpringBootApplication(scanBasePackages = {"no.fjordkraft.im","no.fjordkraft.im.controller","no.fjordkraft.security"})
 @EnableAutoConfiguration(exclude={LiquibaseAutoConfiguration.class})
 @EnableEurekaClient
+@EnableJpaRepositories(basePackages = {"no.fjordkraft.im","no.fjordkraft.security.jpa.repository"})
+@EntityScan(basePackages={"no.fjordkraft.im","no.fjordkraft.security"})
 public class InvoiceManagerUIApplication {
 
 	public static void main(String[] args) {
