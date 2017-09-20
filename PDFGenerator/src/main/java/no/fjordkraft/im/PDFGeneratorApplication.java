76e916aa2f75001a7e8d2c5f37864b272c6dad7d
bhavik shah <bhavik shah>
2017-09-20 13:48:27 +0530

    fix

--- PDFGenerator/src/main/java/no/fjordkraft/im/PDFGeneratorApplication.java
+++ PDFGenerator/src/main/java/no/fjordkraft/im/PDFGeneratorApplication.java
@@ -1,17 +1,20 @@
 package no.fjordkraft.im;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ServletComponentScan({"no.fjordkraft.im"})
-@SpringBootApplication(scanBasePackages = {"no.fjordkraft.im", "no.fjordkraft.im.controller", "no.fjordkraft.security"})
+@SpringBootApplication(scanBasePackages = {"no.fjordkraft.im", "no.fjordkraft.im.controller", "no.fjordkraft.security"},exclude = {MongoAutoConfiguration.class,})
 @EnableEurekaClient
 @EnableJpaRepositories(basePackages = {"no.fjordkraft.im", "no.fjordkraft.security.jpa.repository"})
 @EntityScan(basePackages = {"no.fjordkraft.im", "no.fjordkraft.security"})
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class PDFGeneratorApplication {
 
     public static void main(String[] args) {
