b03c1a9962bba29e4799dbaeaf2615f26546486b
arpita-shah <arpita.shah@miles.in>
2018-05-17 15:52:21 +0530

    Reverted changes back to previous one.

--- Preprocessor/src/main/resources/liquidbase/db-changelog-3.0.0.sql
+++ Preprocessor/src/main/resources/liquidbase/db-changelog-3.0.0.sql
@@ -326,7 +326,7 @@ Insert into IM_TRANSACTION_GRP_CATEGORY (ID,TG_ID,TC_ID) values (259,5,31);
 Insert into IM_TRANSACTION_GRP_CATEGORY (ID,TG_ID,TC_ID) values (260,5,32);
 Insert into IM_TRANSACTION_GRP_CATEGORY (ID,TG_ID,TC_ID) values (261,5,33);
 Insert into IM_TRANSACTION_GRP_CATEGORY (ID,TG_ID,TC_ID) values (262,5,41);
---Adding new transaction category to TKAS Rabatter group.
+--Adding new transaction category to TKAS Rabatter group
 Insert into IM_TRANSACTION_GRP_CATEGORY (ID,TG_ID,TC_ID) values (263,5,185);
 Insert into IM_TRANSACTION_GRP_CATEGORY (ID,TG_ID,TC_ID) values (264,5,186);
 
