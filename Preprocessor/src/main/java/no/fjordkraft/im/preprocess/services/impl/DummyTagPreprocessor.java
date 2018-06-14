6f688fefc840bc7e0453dcfdb6c0be6fc5b565f1
bhavik shah <bhavik.shah@miles.in>
2018-06-14 11:15:46 +0530

    reverted change

--- Preprocessor/src/main/java/no/fjordkraft/im/preprocess/services/impl/DummyTagPreprocessor.java
+++ Preprocessor/src/main/java/no/fjordkraft/im/preprocess/services/impl/DummyTagPreprocessor.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class DummyTagPreprocessor extends BasePreprocessor {
 
     private static final Logger logger = LoggerFactory.getLogger(DummyTagPreprocessor.class);
-    int totalDummyTag = 10;
+    int totalDummyTag = 9;
 
     @Override
     public void preprocess(PreprocessRequest<Statement, no.fjordkraft.im.model.Statement> request) {
