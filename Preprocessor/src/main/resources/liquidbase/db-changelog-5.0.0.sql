d8455a40c815c1d3dc5ba6817a41c9acd748e264
bhavik shah <bhavik.shah@miles.in>
2018-06-22 15:44:48 +0530

    added attachment config

--- Preprocessor/src/main/resources/liquidbase/db-changelog-5.0.0.sql
+++ Preprocessor/src/main/resources/liquidbase/db-changelog-5.0.0.sql
@@ -1,3 +1,4 @@
 --changeset arpita:1 context:prod
 --Added new column to check the legal part class in PDFGenerator.
-alter table im_statement add LEGAL_PART_CLASS VARCHAR2(100 BYTE);
+alter table im_statement add LEGAL_PART_CLASS VARCHAR2(100);
+Insert into INVOICEMAN.IM_ATTACHMENT_CONFIG (ID,ATTACHMENT_NAME) values (4,'ORGANIZATION');
