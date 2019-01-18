33197dccd69107995ed5f1a62f72b0b427692a49
arpita-shah <arpita.shah@miles.in>
2019-01-18 13:15:16 +0530

    Added code to save invoice pdf based on configuration

--- Preprocessor/src/main/java/no/fjordkraft/im/preprocess/services/impl/PDFAttachmentExtractor.java
+++ Preprocessor/src/main/java/no/fjordkraft/im/preprocess/services/impl/PDFAttachmentExtractor.java
@@ -6,6 +6,7 @@ import no.fjordkraft.im.model.InvoicePdf;
 import no.fjordkraft.im.preprocess.models.PreprocessRequest;
 import no.fjordkraft.im.preprocess.models.PreprocessorInfo;
 import no.fjordkraft.im.services.InvoiceService;
+import no.fjordkraft.im.util.IMConstants;
 import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,23 +31,25 @@ public class PDFAttachmentExtractor extends BasePreprocessor {
         Statement stmt = request.getStatement();
         int count = 0;
         for(Attachment attachment : stmt.getAttachments().getAttachment()){
-            if("PDF".equals(attachment.getFAKTURA().getVEDLEGGFORMAT()) || "PDFEHF".equals(attachment.getFAKTURA().getVEDLEGGFORMAT()) || "PDFE2B".equals(attachment.getFAKTURA().getVEDLEGGFORMAT())){
-                InvoicePdf invoicePdf = new InvoicePdf();
-                byte[] decoded = Base64.decodeBase64(attachment.getFAKTURA().getVEDLEGGPDF().getBytes());
-                invoicePdf.setPayload(decoded);
-                invoicePdf.setType(attachment.getFAKTURA().getVEDLEGGFORMAT());
-                invoicePdf.setStatement(request.getEntity());
-                invoicePdf = invoiceService.saveInvoicePdf(invoicePdf);
-                count++;
-                if(!request.getEntity().isE2bAttachment() && "PDFE2B".equals(attachment.getFAKTURA().getVEDLEGGFORMAT()))  {
-                    request.getEntity().setE2bAttachment(true);
-                }
-                if(!request.getEntity().isEhfAttachment() && "PDFEHF".equals(attachment.getFAKTURA().getVEDLEGGFORMAT()))  {
-                    request.getEntity().setEhfAttachment(true);
-                }
+                if("PDF".equals(attachment.getFAKTURA().getVEDLEGGFORMAT()) || "PDFEHF".equals(attachment.getFAKTURA().getVEDLEGGFORMAT()) || "PDFE2B".equals(attachment.getFAKTURA().getVEDLEGGFORMAT())){
+                    if(configService.getBoolean(IMConstants.SAVE_INVOICE_PDF_IN_DB)) {
+                    InvoicePdf invoicePdf = new InvoicePdf();
+                    byte[] decoded = Base64.decodeBase64(attachment.getFAKTURA().getVEDLEGGPDF().getBytes());
+                    invoicePdf.setPayload(decoded);
+                    invoicePdf.setType(attachment.getFAKTURA().getVEDLEGGFORMAT());
+                    invoicePdf.setStatement(request.getEntity());
+                    invoicePdf = invoiceService.saveInvoicePdf(invoicePdf);
+                     }
+                    count++;
+                    if(!request.getEntity().isE2bAttachment() && "PDFE2B".equals(attachment.getFAKTURA().getVEDLEGGFORMAT()))  {
+                        request.getEntity().setE2bAttachment(true);
+                    }
+                    if(!request.getEntity().isEhfAttachment() && "PDFEHF".equals(attachment.getFAKTURA().getVEDLEGGFORMAT()))  {
+                        request.getEntity().setEhfAttachment(true);
+                    }
 
-                //logger.debug("Save pdf of type "+invoicePdf.getType() + " with id "+invoicePdf.getId() + " statement id "+request.getEntity().getId() );
-            }
+                    //logger.debug("Save pdf of type "+invoicePdf.getType() + " with id "+invoicePdf.getId() + " statement id "+request.getEntity().getId() );
+                }
         }
         request.getEntity().setPdfAttachment(count);
     }
