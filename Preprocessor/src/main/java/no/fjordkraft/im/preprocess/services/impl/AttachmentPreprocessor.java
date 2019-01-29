cdef60ad8e1298932b1ae9775c84b71f237528dc
arpita-shah <arpita.shah@miles.in>
2019-01-29 15:23:58 +0530

    IM-214 reverted as it is not required.

--- Preprocessor/src/main/java/no/fjordkraft/im/preprocess/services/impl/AttachmentPreprocessor.java
+++ Preprocessor/src/main/java/no/fjordkraft/im/preprocess/services/impl/AttachmentPreprocessor.java
@@ -831,14 +831,14 @@ public class AttachmentPreprocessor extends BasePreprocessor {
                 }
                 List<Ref> refList = baseItemDetails.getRef();
                //IM-214 : if price is between <1 and >-1 then, it should be multiplied by 100.
-                if(baseItemDetails.getUnitPrice()>-1 && baseItemDetails.getUnitPrice()<1) {
-                    baseItemDetails.setUnitPrice(baseItemDetails.getUnitPrice()/100);
+               /* if(baseItemDetails.getUnitPrice()>-1 && baseItemDetails.getUnitPrice()<1) {
+                   // baseItemDetails.setUnitPrice(baseItemDetails.getUnitPrice()/100);
                     baseItemDetails.setUnitPriceGross(baseItemDetails.getUnitPrice()/100);
                 }
                 else {
                 baseItemDetails.setUnitPriceGross(baseItemDetails.getUnitPrice());
-                }
-               // baseItemDetails.setUnitPriceGross(baseItemDetails.getUnitPrice());
+                }*/
+                baseItemDetails.setUnitPriceGross(baseItemDetails.getUnitPrice());
                 baseItemDetails.setAttachmentFormat(pdfAttachment.getFAKTURA().getVEDLEGGFORMAT());
                 if (null != refList) {
                     for (Ref ref : refList) {
